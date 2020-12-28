28a817672ac91b94246dd4e8180b7d98cda165fb
gdasdores <76019243+gdasdores@users.noreply.github.com>
2020-12-28 20:39:20 -0300

    V2

--- calculaDores.js
+++ calculaDores.js
@@ -1,12 +1,12 @@
 const { parse } = require("path");
 
-
 var inputMoneda;
 var outputText;
 
 async function validate() {
 
     // get the input
+
     inputMoneda = document.forms["input_form"]["elInput"].value;
 
     var peticionApi = await fetch('https://api.exchangerate.host/latest?base=USD');
@@ -22,14 +22,16 @@ async function validate() {
     } else {
         // calculate the result
 
+
         var dolarOficial = peticionApiJSON.rates.ARS.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         var dolarBlue = 164.00;
 
         var today = new Date();
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
+        var dolarAhorroUnidad = (dolarOficial * 1.65).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         var dolarAhorro = (inputMoneda * (dolarOficial * 1.65)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        outputText = "Tu compra por un total de <strong>$ " + inputMoneda.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " dólares </strong> va a costarte <strong>$ " + dolarAhorro + " pesos</strong>, y la cotización oficial al día <strong>" + date +  " </strong> es de <strong>$ " + dolarOficial + " pesos.</strong>";
+        outputText = "<font size='+3'><strong>$" + dolarAhorro + "</font></strong><br/><br/> Tu compra por un total de <strong>$ " + inputMoneda.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " dólares </strong> va a costarte <strong>$ " + dolarAhorro + " pesos</strong>. <br/> La cotización oficial del dólar al día <strong>" + date + " </strong> es de <strong>$ " + dolarOficial + " pesos</strong>, y el dólar ahorro queda en <strong>$" + dolarAhorroUnidad + " pesos</strong>.";
 
     }
 
