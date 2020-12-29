feba46b7e985f3f751446add07afb043a1889ec6
gdasdores <76019243+gdasdores@users.noreply.github.com>
2020-12-29 14:18:05 -0300

    v3

--- calculaDores.js
+++ calculaDores.js
@@ -31,7 +31,7 @@ async function validate() {
 
         var dolarAhorroUnidad = (dolarOficial * 1.65).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         var dolarAhorro = (inputMoneda * (dolarOficial * 1.65)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        outputText = "<font size='+3'><strong>$" + dolarAhorro + "</font></strong><br/><br/> Tu compra por un total de <strong>$ " + inputMoneda.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " dólares </strong> va a costarte <strong>$ " + dolarAhorro + " pesos</strong>. <br/> La cotización oficial del dólar al día <strong>" + date + " </strong> es de <strong>$ " + dolarOficial + " pesos</strong>, y el dólar ahorro queda en <strong>$" + dolarAhorroUnidad + " pesos</strong>.";
+        outputText = "<font size='+3'><strong>$" + dolarAhorro + "</font></strong><br/><br/> Tu compra por un total de <strong>$ " + inputMoneda.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " dólares </strong> va a costarte <strong>$ " + dolarAhorro + " pesos</strong>. <br/> La cotización oficial del dólar al día <strong>" + date + " </strong> es de <strong>$ " + dolarOficial + " pesos</strong>, y la del dólar ahorro es de <strong>$" + dolarAhorroUnidad + " pesos</strong>.";
 
     }
 
